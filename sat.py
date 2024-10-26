1a48156508a5278a1f58a0028696e2c6a78df6c9
Anant Khandelwal <anantk2006@gmail.com>
2024-10-25 22:53:52 -0400

    functional, needs more testing

--- sat.py
+++ sat.py
@@ -1,11 +1,37 @@
-import pandas as pd
-from datetime import datetime as dt
-import matplotlib.pyplot as plt
-from IPython.display import display
-
 from arcgis.gis import GIS
-from arcgis.features.analyze_patterns import calculate_density, find_hot_spots
-from arcgis.features.use_proximity import create_drive_time_areas
-from arcgis.features.summarize_data import summarize_within
+from arcgis.layers import MapFeatureLayer
+from arcgis.geometry import Point
+from arcgis.geometry.filters import intersects
 
 gis = GIS()
+
+one = gis.content.get("1311369dea53479ca9608cb5abca565f")
+three = gis.content.get("365fa1f7f5314c0c81af516028f9c928")
+map_feature_layer = MapFeatureLayer.fromitem(item = three)
+
+
+
+
+query_extent = {
+    "xmin": -11908433.363109391 - 1,
+    "ymin": 5352150.796410866 - 1,
+    "xmax": -11908433.363109391 + 1,
+    "ymax": 5352150.796410866 + 1,
+    "spatialReference": {"wkid": 102100},
+}
+
+query_filter = intersects(query_extent, sr=102100)
+
+results = map_feature_layer.query(
+    geometry_filter=query_filter, out_fields="*", as_df=False
+)
+for feature in results.features:
+    print(feature.attributes)
+print()
+
+# print(results)
+# m1 = gis.map("Miami, Florida")
+# m1.content.add(three)
+# display(m1)
+
+
